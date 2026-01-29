8a6011ba4b260e62f61f9fb5d76ec9aea1836c6a
Jamal Salem <jaudi990@yahoo.com>
2026-01-29 23:27:28 +0400

    hello world C program
    Initial C program to test the build environment.

---
+++ basics/basics/hello_world/basics/hello_world/main.c
@@ -0,0 +1,16 @@
+/*
+* file: main.c
+* Author: Jamal Salem
+* Description: First C program to test build environment
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    // print a message
+    printf("Hello, World!\n");
+
+    return (EXIT_SUCCESS);
+}
